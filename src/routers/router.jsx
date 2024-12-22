017d4d91b1440514046aa9d7341a145af42d8560
saagor16 <saagor.16@gmail.com>
2024-12-23 00:04:16 +0600

    Set up nested routing with Outlet in router.jsx

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,9 +1,16 @@
 import {createBrowserRouter} from "react-router-dom";
+import App from "../App";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element:<App></App>,
+      children:[
+        {
+            path: '/',
+            element : <h1>home</h1>
+        }
+      ]
     },
   ]);
 
