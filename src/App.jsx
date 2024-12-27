f20994a9ddcc23f362a80eb57b6d1a4d743aeec8
saagor16 <saagor.16@gmail.com>
2024-12-27 06:27:26 +0600

    Add multiple CSS styles to App.css from GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <>
-      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
+      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet></Outlet>
       </main>
     </>
