984dcf8a7bdc1b67f19e1ad4728c78375498cf93
saagor16 <saagor.16@gmail.com>
2024-12-27 15:03:43 +0600

    Complete full UI design excluding login and logout functionalities

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,19 @@ import {
 } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import avatarImg from "../assets/avatar.png";
+import { useState } from "react";
+const navigation = [
+  { name: "Dashboard", href: "/user-dashboard" },
+  { name: "Orders", href: "/orders" },
+  { name: "Cart Page", href: "/cart" },
+  { name: "Check Out", href: "/checkout" },
+];
 
 const Navbar = () => {
-  const currentUser = false;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const currentUser = true;
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
@@ -33,9 +43,31 @@ const Navbar = () => {
         <div className="relative flex items-center sm:space-x-3 space-x-2">
           <div>
             {currentUser ? (
-              <>user</>
+              <>
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  <img
+                    src={avatarImg}
+                    alt=""
+                    className={`size-7 rounded-full ${
+                      currentUser ? "ring-2 ring-blue-500" : ""
+                    }`}
+                  />
+                </button>
+
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
+                      {navigation.map((item) => (
+                        <li key={item.name} onClick={() => setIsDropdownOpen(false)}>
+                          <Link to={item.href}  className="block px-4 py-2 text-sm hover:bg-gray-100">{item.name}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
             ) : (
-              <Link to='/login'>
+              <Link to="/login">
                 <HiOutlineUser className="size-6"></HiOutlineUser>
               </Link>
             )}
