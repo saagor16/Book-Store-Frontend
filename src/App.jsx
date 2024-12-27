7c73e53069fa6a905f82c0bd895a300da43c941c
saagor16 <saagor.16@gmail.com>
2024-12-27 15:39:20 +0600

    banner section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import './App.css'
+
 
 function App() {
   return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "./Banner"
 
 const Home = () => {
   return (
     <>
-      home
+      <Banner></Banner>
     </>
   )
 }
