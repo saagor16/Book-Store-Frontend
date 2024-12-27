113ae4aa032dc820fea9678929b1b020971472c6
saagor16 <saagor.16@gmail.com>
2024-12-27 06:36:17 +0600

    navbar file create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar></Navbar>
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet></Outlet>
       </main>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Navbar = () => {
+  return (
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+        
+    </header>
+  )
+}
+
+export default Navbar
