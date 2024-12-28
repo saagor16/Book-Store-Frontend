d0a8bba5e3baad0c020daf8f0661073bf7efd097
saagor16 <saagor.16@gmail.com>
2024-12-28 12:50:55 +0600

    register page er ui degine done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,31 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { FaGoogle } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const [message, setMessage] = useState("");
+
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit =(data) =>console.log(data);
+  const handleGoogleSignIn = () => {
+    
+  }
+
   return (
     <div className="h-[calc(100vh-120px)] flex justify-center items-center ">
       <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h2 className="text-xl font-semibold mb-4">Please Login</h2>
 
-        <form >
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -16,6 +34,7 @@ const Login = () => {
               Email
             </label>
             <input
+              {...register("email", { required: true })}
               type="email"
               name="email"
               id="email"
@@ -31,6 +50,7 @@ const Login = () => {
               Password
             </label>
             <input
+              {...register("password", { required: true })}
               type="password"
               name="password"
               id="password"
@@ -38,7 +58,10 @@ const Login = () => {
               className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
             />
           </div>
-         
+          {message && (
+            <p className="text-red-500 text-xs italic mb-3">{message}</p>
+          )}
+
           <div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none">
               Login{" "}
@@ -55,8 +78,8 @@ const Login = () => {
         {/* google sign in */}
         <div className="mt-4">
           <button
-            className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
-          >
+          onClick={handleGoogleSignIn}         
+           className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
             <FaGoogle className="mr-2" />
             Sign in with Google
           </button>
