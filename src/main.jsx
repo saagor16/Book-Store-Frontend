d63cba1117183e60f8a8bab9db5da6b652c2f061
saagor16 <saagor.16@gmail.com>
2024-12-28 22:59:58 +0600

    add to cart btn e swel alert add

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import router from "./routers/router.jsx";
 import { RouterProvider } from "react-router-dom";
+import  'sweetalert2/dist/sweetalert2.js'
+
 
 import { Provider } from 'react-redux'
 import { store } from "./redux/store.js";
--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
 
 const initialState = {
   cartItems: [],
@@ -12,17 +13,37 @@ const cartSlice = createSlice({
       const existingItem = state.cartItems.find(
         (item) => item._id === action.payload._id
       );
-      if(!existingItem){
-        state.cartItems.push(action.payload)
-        alert("Item added successfully")
-      }else(
-        alert("item Already exists")
-      )
+      if (!existingItem) {
+        state.cartItems.push(action.payload);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Product Added to the Cart",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      } else
+        Swal.fire({
+          title: "Already Added to the Cart",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "OK!",
+        });
     },
-
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+    },
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
   },
 });
 
-// export the actions 
+// export the actions
 export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
