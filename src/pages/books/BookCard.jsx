c349a08899dd12ece3fea28ee7d93a3cd99fd7f1
saagor16 <saagor.16@gmail.com>
2024-12-28 18:36:32 +0600

    bookcart e addtocart addes

--- src/pages/books/BookCard.jsx
+++ src/pages/books/BookCard.jsx
@@ -2,8 +2,15 @@
 import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { getImgUrl } from "../../utils/getImgUrl";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 
 const BookCard = ({ book }) => {
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
   return (
     <div className=" rounded-lg transition-shadow duration-300">
       <div className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4">
@@ -34,7 +41,10 @@ const BookCard = ({ book }) => {
               $ {book?.oldPrice}
             </span>
           </p>
-          <button className="btn-primary px-6 space-x-1 flex items-center gap-1 ">
+          <button
+            onClick={() => handleAddToCart(book)}
+            className="btn-primary px-6 space-x-1 flex items-center gap-1 "
+          >
             <FiShoppingCart className="w-5 h-5" />
             <span>Add to Cart</span>
           </button>
