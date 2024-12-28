aedf2b7ffc66acdfbc5f55030b08e3b0485845f6
saagor16 <saagor.16@gmail.com>
2024-12-28 12:25:41 +0600

    register fgile create and added routes

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../components/Login";
+import Register from "../components/Register";
 
   const router = createBrowserRouter([
     {
@@ -19,6 +20,10 @@ import Login from "../components/Login";
         {
             path: '/login',
             element :<Login></Login>
+        },
+        {
+            path: '/register',
+            element :<Register></Register>
         },
       ]
     },
